bb7297f8e052d37b5c28e3ea6b50605e95b7738a
Yasharth Shukla <borosoverlord001@gmail.com>
2025-02-03 16:02:28 +0530

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<div class="footer">
+    <p>&copy; 2025 Neighbourly <img src="./assets/images/logo.png" class="footer-logo" alt="">. All rights reserved.</p>
+    <nav>
+        <a href="/privacy-policy">Privacy Policy</a> | 
+        <a href="/terms-of-service">Terms of Service</a> | 
+        <a href="/contact">Contact Us</a>
+    </nav>
+</div>
