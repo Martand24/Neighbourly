3014ff91b3850013517ef299c0aaf02c56f52a8f
Aashna Bara <aashnarose12@gmail.com>
2025-02-03 18:51:30 +0530

    Update home.php

--- home.php
+++ home.php
@@ -11,28 +11,7 @@
     <title>Neighbourly</title>
 </head>
 <body>
-    <div class="nav-bar">
-        <div class="no-collapse">
-            <div>
-                <img src="./assets/images/logo.png" class="logo" alt="logo">
-            </div>
-            <a class="nav-share" href="/post.php">Share</a>
-            <a class="nav-explore" href="/explore.php">Explore</a>
-            <div class="search-bar">
-                <form  class="search-bar-inner" action="search_results.php" method="get">
-                    <input type="text" placeholder="Search items" class="search-box">
-                    <button class="search-button" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
-                </form>
-            </div>
-        </div>
-        <div class="myCollapse">
-            <a class="nav-about" href="/aboutUs.php">About us</a>
-            <a class="nav-contact" href="/contactUs.php">Contact Us</a>
-            <a class="nav-profile" href="/user.php">
-                <img src="/profile.png" class="nav-profile" alt="">
-            </a>
-        </div>
-    </div>
+    <?php require './includes/navbar.php'; ?>
 
     <div class="header">
         <div class="text-box">
@@ -109,14 +88,7 @@
         </div>
     </div>
 
-    <div class="footer">
-      <p>&copy; 2025 Neighbourly <img src="/logo.png" class="footer-logo" alt="">. All rights reserved.</p>
-      <nav>
-          <a href="/privacy-policy">Privacy Policy</a> | 
-          <a href="/terms-of-service">Terms of Service</a> | 
-          <a href="/contact">Contact Us</a>
-      </nav>
-    </div>
+     <?php require './includes/footer.php'; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
