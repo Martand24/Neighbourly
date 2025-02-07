135c08a322b773035948649ee013101dea589c71
Anuj Dhurve <sanujdhurve12@gmail.com>
2025-02-07 23:10:12 +0530

    Update post.php

--- post.php
+++ post.php
@@ -89,3 +89,112 @@
 </body>
 
 </html>
+
+
+<?php
+$servername = "your_servername";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_dbname";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   
+    $category = mysqli_real_escape_string($conn, $_POST['category']);
+    $title = mysqli_real_escape_string($conn, $_POST['title']);
+    $description = mysqli_real_escape_string($conn, $_POST['description']);
+    $location = mysqli_real_escape_string($conn, $_POST['location']);
+    $condition = mysqli_real_escape_string($conn, $_POST['condition']);
+
+    
+    $targetDir = "uploads/";
+
+    
+    if (!is_dir($targetDir)) {
+        echo "Directory does not exist. Please create the 'uploads' directory.";
+        exit;
+    }
+
+    if (!is_writable($targetDir)) {
+        echo "Directory is not writable. Please check the permissions of the 'uploads' directory.";
+        exit;
+    }
+
+    $uploadedFiles = [];
+    $uploadOk = 1;
+    $errorMsg = '';
+
+    foreach ($_FILES["imageUpload"]["name"] as $key => $name) {
+        
+        $targetFile = $targetDir . uniqid() . "_" . basename($name);
+        $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+       
+        $check = getimagesize($_FILES["imageUpload"]["tmp_name"][$key]);
+        if ($check === false) {
+            $uploadOk = 0;
+            $errorMsg = "Invalid image file.";
+            break;
+        }
+
+       
+        if ($_FILES["imageUpload"]["size"][$key] > 5000000) {
+            $uploadOk = 0;
+            $errorMsg = "File too large (5MB limit).";
+            break;
+        }
+
+       
+        if (!in_array($imageFileType, ["jpg", "png", "jpeg", "gif"])) {
+            $uploadOk = 0;
+            $errorMsg = "Invalid file type.";
+            break;
+        }
+
+        
+        if ($_FILES["imageUpload"]["error"][$key] !== UPLOAD_ERR_OK) {
+            $uploadOk = 0;
+            $errorMsg = "Upload error: " . $_FILES["imageUpload"]["error"][$key];
+            break;
+        }
+
+        
+        if ($uploadOk == 1) {
+            if (move_uploaded_file($_FILES["imageUpload"]["tmp_name"][$key], $targetFile)) {
+                $uploadedFiles[] = $targetFile;  
+            } else {
+                $uploadOk = 0;
+                $errorMsg = "Sorry, there was an error uploading your file.";
+                break;
+            }
+        }
+    }
+
+   
+    if ($uploadOk == 1) {
+        $filePaths = implode(",", $uploadedFiles); 
+        $sql = "INSERT INTO posts (category, title, description, location, condition, images)
+                VALUES ('$category', '$title', '$description', '$location', '$condition', '$filePaths')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "Post submitted successfully!";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    } else {
+        
+        echo "<div class='error'>" . $errorMsg . "</div>";
+    }
+}
+
+$conn->close();
+?>
+
+
