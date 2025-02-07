aa08c8491a11cf7bfc7635142e891fdb5dc09d27
Anuj Dhurve <sanujdhurve12@gmail.com>
2025-02-07 23:19:27 +0530

    Update post.php

--- post.php
+++ post.php
@@ -90,20 +90,9 @@
 
 </html>
 
-
 <?php
-$servername = "your_servername";
-$username = "your_username";
-$password = "your_password";
-$dbname = "your_dbname";
 
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include("connection.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
    
