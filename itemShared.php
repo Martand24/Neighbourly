766f81c82381810d2b42bb099abc3870ce864116
Aashna Bara <aashnarose12@gmail.com>
2025-02-16 21:10:59 +0530

    Update itemShared.php

--- itemShared.php
+++ itemShared.php
@@ -4,36 +4,17 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">    
         <link rel="icon"  href="/logo.png">
-        <link rel="stylesheet" href="/itemShared.css">
+        <link rel="stylesheet" href="./assets/css/itemShared.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <title>Neighbourly</title>
     </head> 
 <body>
-    <div class="nav-bar">
-        <div class="no-collapse">
-            <div>
-                <img src="/logo.png" class="logo" alt="logo">
-            </div>
-            <a class="nav-share" href="/post.php">Share</a>
-            <a class="nav-explore" href="/index1.html">Home</a>
-            <div class="search-bar">
-                <form  class="search-bar-inner" action="search_results.php" method="get">
-                    <input type="text" placeholder="Search items" class="search-box">
-                    <button class="search-button" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
-                </form>
-            </div>
-        </div>
-        <div class="myCollapse">
-            <a class="nav-about" href="/aboutUs.html">About us</a>
-            <!-- <a class="nav-contact" href="/contactUs.php">Contact Us</a> -->
-            <a class="nav-profile" href="/user.php">
-                <img src="/profile.png" class="nav-profile" alt="">
-            </a>
-            <!-- <p class="lgn-sgn"><a href="/login.php">Register</a></p> -->
-        </div>
-    </div>
+    <!--navbar-->
+    <?php require './includes/navbar.php'; ?>
+
+    <!--shared items-->
 
     <p style="font-size: 40px; margin-left: 2rem; font-weight: bold;">Items Shared</p>
     <div class="shared-items">
@@ -95,13 +76,6 @@
 
 
     <!-- footer -->
-    <div class="footer">
-        <p>&copy; 2025 Neighbourly <img src="/logo.png" class="footer-logo" alt="">. All rights reserved.</p>
-        <nav>
-            <a href="/privacy-policy">Privacy Policy</a> | 
-            <a href="/terms-of-service">Terms of Service</a> | 
-            <a href="/contact">Contact Us</a>
-        </nav>
-      </div>
+     <?php require './includes/footer.php'; ?>
 </body>
 </html>
