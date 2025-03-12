0b6e4205a60c2d6958db22349eb55ce1cde68f5a
Yasharth Shukla <borosoverlord001@gmail.com>
2025-03-13 00:22:23 +0530

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,3 +1,4 @@
+
 <?php
 if (session_status() === PHP_SESSION_NONE) {
     session_start(); // Start session only if it's not already started
@@ -9,24 +10,44 @@ if (session_status() === PHP_SESSION_NONE) {
         <div>
             <img src="./assets/images/logo.png" class="logo" alt="logo">
         </div>
-        <a class="nav-share" href="/post.php">Share</a>
-        <a class="nav-explore" href="/items.php">Explore</a>
+        <a class="nav-share" href="./post.php">Share</a>
+        <a class="nav-explore" href="./explore.php">Explore</a>
         <div class="search-bar">
-            <form  class="search-bar-inner" action="search_results.php" method="get">
-                <input type="text" placeholder="Search items" class="search-box">
-                <button class="search-button" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+            <form  class="search-bar-inner" action="/neighbourly/explore.php" method="get">
+                <input type="text" placeholder="Search items" name="query" class="search-box" id="search_input">
+                <button class="search-button" type="submit" ><i class="fa-solid fa-magnifying-glass"></i></button>
             </form>
         </div>
     </div>
     <div class="myCollapse">
         <a class="nav-about" href="/aboutUs.html">About us</a>
         <!-- <a class="nav-contact" href="/contactUs.php">Contact Us</a> -->
-        <?php if (isset($_SESSION['id'])): ?>
-		<a class="nav-profile" href="./profile.php">
-	            <img style="height:2rem; width:2rem" src="/profile.png" class="nav-profile" alt="">
-		</a>
-        <?php else: ?>
-		<a href="./login.php">Login In</a>
-        <?php endif; ?>
+        <?php if (isset($_SESSION['id'])): 
+    $id = $_SESSION['id'];
+    include("connection.php"); // Ensure database connection is included
+    $query = mysqli_query($conn, "SELECT photo FROM users WHERE id = $id") or die("Error occurs");
+
+    $res_photo = ""; // Default empty
+    if ($result = mysqli_fetch_assoc($query)) {
+        $res_photo = $result['photo'];
+    }
+?>
+    <a class="nav-profile" href="./profile.php">
+        <img style="height:3rem; width:3rem; border-radius: 50%;" 
+             src="<?php echo !empty($res_photo) ? $res_photo : './assets/images/logo.png'; ?>" 
+             class="nav-profile" 
+             alt="User Photo">
+    </a>
+<?php else: ?>
+    <a href="./login.php">Login</a>
+<?php endif; ?>
+
+
+
+
+
+
+        
     </div>
 </div>
+
