89dfcbf77098c4d536caaccc603409c329058d0f
Aashna Bara <aashnarose12@gmail.com>
2025-03-20 08:49:57 +0530

    Create aboutUs.php

---
+++ aboutUs.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+        <link rel="icon"  href="./assets/css/logo.png">
+  <link rel="stylesheet" href="./assets/css/navbar.css">
+	<link rel="stylesheet" href="./assets/css/aboutUs.css">
+  <link rel="stylesheet" href="./assets/css/footer.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+<?php require './includes/navbar.php'; ?>
+
+<div class="container-one">
+        <div class="about-us">
+            <div style="margin-bottom: 1rem;" class="heading">Neighbourly</div>
+            <div class="sub-heading">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quod autem ipsam a, similique eos corrupti dolore sint quaerat blanditiis? Corporis aut blanditiis, rem et nemo atque laboriosam nulla fuga.Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel eos dignissimos aut ducimus, cum iusto dolores voluptate repellendus nostrum cumque praesentium eveniet vero. Eveniet eum alias magni reprehenderit possimus error!
+
+            </div>
+        </div>
+        <div class="logo-box">
+            <img src="/logo.png" alt="">
+        </div>
+     </div>
+
+     <div class="container-two">
+        <div style=" margin-top: 4rem; margin-bottom: 4rem; "class="heading">Our Story</div>
+        <div style="width: 80%;" class="sub-heading">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur doloribus illo laboriosam voluptate molestias corrupti repudiandae velit quibusdam libero, obcaecati possimus ab at est quo hic beatae facilis aspernatur vitae! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore impedit magnam quidem dolorem itaque facere sunt numquam voluptatum quaerat tempora obcaecati quam corrupti repellat velit eligendi perferendis, consectetur a. Aut?</div>
+
+        <div style="width: 80%; margin-top: 10px;" class="sub-heading">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur doloribus illo laboriosam voluptate molestias corrupti repudiandae velit quibusdam libero, obcaecati possimus ab at est quo hic beatae facilis aspernatur vitae! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore impedit magnam quidem dolorem itaque facere sunt numquam voluptatum quaerat tempora obcaecati quam corrupti repellat velit eligendi perferendis, consectetur a. Aut?</div>
+
+        <div style="width: 80%; margin-top: 10px; margin-bottom: 40px;" class="sub-heading">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur doloribus illo laboriosam voluptate molestias corrupti repudiandae velit quibusdam libero, obcaecati possimus ab at est quo hic beatae facilis aspernatur vitae! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore impedit magnam quidem dolorem itaque facere sunt numquam voluptatum quaerat tempora obcaecati quam corrupti repellat velit eligendi perferendis, consectetur a. Aut?</div>
+
+
+     </div>
+
+     <div class="container-three">
+        <div style="font-size: 80px; font-weight: bold; font-family: serif;"> LOREM LOREM LOREM</div>
+        <img src="/bottle.png" alt="">
+
+     </div>
+<?php require './includes/footer.php'; ?>
+
+</body>
+</html>
